684dd8a5f40ac43785a2d76b3420c09f4a63b3ac
Peter Schutt <peter.github@proton.me>
2022-08-22 08:38:51 +1000

    Upgrade to Starlite 1.13

--- src/starlite_lib/starlite/compression.py
+++ src/starlite_lib/starlite/compression.py
@@ -1,4 +1,4 @@
-from starlite.config import CompressionBackend, CompressionConfig
+from starlite.config.compression import CompressionBackend, CompressionConfig
 
 config = CompressionConfig(backend=CompressionBackend.GZIP)
 """Default compression config"""
