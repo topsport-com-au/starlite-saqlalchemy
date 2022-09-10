c9ebe2522a97ffe0e59c70c2420486df94a48abb
Peter Schutt <peter.github@proton.me>
2022-09-10 17:00:57 +1000

    Explicit transaction management for db health check.

--- src/starlite_lib/starlite/health.py
+++ src/starlite_lib/starlite/health.py
@@ -9,5 +9,7 @@ from . import db
 @get(path=api_settings.HEALTH_PATH, cache=False, tags=["Misc"])
 async def health_check() -> AppSettings:
     """Check database available and returns app config info."""
-    assert (await db.AsyncScopedSession().execute(text("SELECT 1"))).scalar_one() == 1
+    async with db.AsyncScopedSession() as session:
+        async with session.begin():
+            assert (await session.execute(text("SELECT 1"))).scalar_one() == 1
     return app_settings
