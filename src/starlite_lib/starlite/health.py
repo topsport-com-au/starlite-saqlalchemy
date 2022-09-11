9cc91063419beb986260b8be38706e9353551029
Peter Schutt <peter.github@proton.me>
2022-09-11 18:12:35 +1000

    Use session dependency in health check.

--- src/starlite_lib/starlite/health.py
+++ src/starlite_lib/starlite/health.py
@@ -1,15 +1,12 @@
 from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlite import get
 
 from starlite_lib.config import AppSettings, api_settings, app_settings
 
-from . import db
-
 
 @get(path=api_settings.HEALTH_PATH, cache=False, tags=["Misc"])
-async def health_check() -> AppSettings:
+async def health_check(session: AsyncSession) -> AppSettings:
     """Check database available and returns app config info."""
-    async with db.AsyncScopedSession() as session:
-        async with session.begin():
-            assert (await session.execute(text("SELECT 1"))).scalar_one() == 1
+    assert (await session.execute(text("SELECT 1"))).scalar_one() == 1
     return app_settings
