425d60781cadd8c76bdfca8d65be33f41d2884d3
Peter Schutt <peter.github@proton.me>
2022-11-06 09:04:35 +1000

    feat(worker): improves the worker pattern. (#90)
    Adds docs.
    
    Closes #14
    Closes #89

--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -1,8 +1,6 @@
 """Tests for Service object patterns."""
-from datetime import date, datetime
 from typing import TYPE_CHECKING, Any
 from unittest.mock import AsyncMock
-from uuid import UUID
 
 import orjson
 
@@ -13,29 +11,20 @@ if TYPE_CHECKING:
     from pytest import MonkeyPatch
 
 
-def test_internal_dto_cache() -> None:
-    """Test ensures that the test domain has an entry in the dto cache for the
-    Author model."""
-    # pylint: disable=protected-access
-    assert domain.Service in service.Service._INTERNAL_DTO_CACHE
-    assert service.Service._INTERNAL_DTO_CACHE[domain.Service].__name__ == "__authorDTO"
-
-
 async def test_make_service_callback(
     raw_authors: list[dict[str, Any]], monkeypatch: "MonkeyPatch"
 ) -> None:
     """Tests loading and retrieval of service object types."""
     recv_cb_mock = AsyncMock()
-    monkeypatch.setattr(service.Service, "receive_callback", recv_cb_mock)
+    monkeypatch.setattr(service.Service, "receive_callback", recv_cb_mock, raising=False)
     await service.make_service_callback(
         {},
         service_module_name="tests.utils.domain",
         service_type_fqdn="Service",
-        operation=service.Operation.UPDATE,
+        service_method_name="receive_callback",
         raw_obj=orjson.loads(orjson.dumps(raw_authors[0], default=str)),
     )
     recv_cb_mock.assert_called_once_with(
-        service.Operation.UPDATE,
         raw_obj={
             "id": "97108ac1-ffcb-411d-8b1e-d9183399f63b",
             "name": "Agatha Christie",
@@ -46,26 +35,16 @@ async def test_make_service_callback(
     )
 
 
-async def test_enqueue_service_callback(
-    raw_authors: list[dict[str, Any]], monkeypatch: "MonkeyPatch"
-) -> None:
+async def test_enqueue_service_callback(monkeypatch: "MonkeyPatch") -> None:
     """Tests that job enqueued with desired arguments."""
     enqueue_mock = AsyncMock()
     monkeypatch.setattr(worker.queue, "enqueue", enqueue_mock)
     service_instance = domain.Service(session=sqlalchemy_plugin.async_session_factory())
-    await service_instance.enqueue_callback(
-        service.Operation.UPDATE, domain.Author(**raw_authors[0])
-    )
+    await service_instance.enqueue_background_task("receive_callback", raw_obj={"a": "b"})
     enqueue_mock.assert_called_once_with(
         "make_service_callback",
         service_module_name="tests.utils.domain",
         service_type_fqdn="Service",
-        operation=service.Operation.UPDATE,
-        raw_obj={
-            "id": UUID("97108ac1-ffcb-411d-8b1e-d9183399f63b"),
-            "created": datetime(1, 1, 1, 0, 0),
-            "updated": datetime(1, 1, 1, 0, 0),
-            "name": "Agatha Christie",
-            "dob": date(1890, 9, 15),
-        },
+        service_method_name="receive_callback",
+        raw_obj={"a": "b"},
     )
