6ef6f54307d91e17dbbf3b0c0a8bc12ea44b3f28
Peter Schutt <peter.github@proton.me>
2022-11-07 08:49:50 +1000

    fix!: removes static files config.
    Will re-implement when there is a concrete use case.
    
    Closes #47

--- src/starlite_saqlalchemy/constants.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the application."""
-
-STATIC_DIR = "static"
-"""Name of the static directory"""
-STATIC_PATH = "/static"
-"""Path static files are served from"""
-FAVICON_PATH = f"{STATIC_PATH}/starlite-favicon.ico"
-"""Name of the favicon file in the static directory"""
