d81e6c6df972c5d8d919e8d9c398404b57acfd8f
Peter Schutt <peter.github@proton.me>
2022-11-17 17:07:23 +1000

    feat(repository): __class_getitem__ sets `model_type` on repo.

--- src/starlite_saqlalchemy/repository/types.py
+++ src/starlite_saqlalchemy/repository/types.py
@@ -1,7 +1,7 @@
 """Repository type definitions."""
 from __future__ import annotations
 
-from typing import Any
+from typing import TYPE_CHECKING, Any, Protocol, TypeVar
 
 from starlite_saqlalchemy.repository.filters import (
     BeforeAfter,
@@ -9,5 +9,30 @@ from starlite_saqlalchemy.repository.filters import (
     LimitOffset,
 )
 
+if TYPE_CHECKING:
+    from pydantic import BaseModel
+
 FilterTypes = BeforeAfter | CollectionFilter[Any] | LimitOffset
 """Aggregate type alias of the types supported for collection filtering."""
+
+
+T = TypeVar("T")
+
+
+class ModelProtocol(Protocol):  # pylint: disable=too-few-public-methods
+    """Protocol for repository models."""
+
+    @classmethod
+    def from_dto(cls: type[T], dto_instance: BaseModel) -> T:  # pragma: no cover
+        """
+
+        Args:
+            dto_instance: A pydantic model.
+
+        Returns:
+            Instance of type with values populated from `dto_instance`.
+        """
+        ...  # pylint: disable=unnecessary-ellipsis
+
+
+ModelT = TypeVar("ModelT", bound=ModelProtocol)
