05bbfade7ed922ee0ff213fb82093fd79dda55b8
Peter Schutt <peter.github@proton.me>
2023-01-11 15:15:48 +1000

    feat(worker): adds job config.
    Default job config can be setup by environment, and individual jobs can
    be explicitly configured with `JobConfig`.
    
    Closes #169

--- src/starlite_saqlalchemy/settings.py
+++ src/starlite_saqlalchemy/settings.py
@@ -255,6 +255,44 @@ class HTTPClientSettings(BaseSettings):
     EXPONENTIAL_BACKOFF_MULTIPLIER: float = 1
 
 
+class WorkerSettings(BaseSettings):
+    """Global SAQ Job configuration."""
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+        env_prefix = "WORKER_"
+
+    JOB_TIMEOUT: int = 10
+    """Max time a job can run for, in seconds.
+
+    Set to `0` for no timeout.
+    """
+    JOB_HEARTBEAT: int = 0
+    """Max time a job can survive without emitting a heartbeat. `0` to disable.
+
+    `job.update()` will trigger a heartbeat.
+    """
+    JOB_RETRIES: int = 10
+    """Max attempts for any job."""
+    JOB_TTL: int = 600
+    """Lifetime of available job information, in seconds.
+
+    0: indefinite
+    -1: disabled (no info retained)
+    """
+    JOB_RETRY_DELAY: float = 1.0
+    """Seconds to delay before retrying a job."""
+    JOB_RETRY_BACKOFF: bool | float = 60
+    """If true, use exponential backoff for retry delays.
+
+    - The first retry will have whatever retry_delay is.
+    - The second retry will have retry_delay*2. The third retry will have retry_delay*4. And so on.
+    - This always includes jitter, where the final retry delay is a random number between 0 and the calculated retry delay.
+    - If retry_backoff is set to a number, that number is the maximum retry delay, in seconds."
+    """
+
+
 # `.parse_obj()` thing is a workaround for pyright and pydantic interplay, see:
 # https://github.com/pydantic/pydantic/issues/3753#issuecomment-1087417884
 api = APISettings.parse_obj({})
@@ -275,3 +313,5 @@ sentry = SentrySettings.parse_obj({})
 """Sentry settings."""
 server = ServerSettings.parse_obj({})
 """Server settings."""
+worker = WorkerSettings.parse_obj({})
+"""Worker settings."""
--- src/starlite_saqlalchemy/worker.py
+++ src/starlite_saqlalchemy/worker.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import asyncio
+import dataclasses
 from functools import partial
 from typing import TYPE_CHECKING, Any
 
@@ -9,16 +10,18 @@ import msgspec
 import saq
 from starlite.utils.serialization import default_serializer
 
-from starlite_saqlalchemy import redis, settings, type_encoders
+from starlite_saqlalchemy import redis, settings, type_encoders, utils
 
 if TYPE_CHECKING:
     from collections.abc import Awaitable, Callable, Collection
     from signal import Signals
 
 __all__ = [
+    "JobConfig",
     "Queue",
     "Worker",
     "create_worker_instance",
+    "default_job_config_dict",
     "queue",
 ]
 
@@ -65,6 +68,53 @@ queue = Queue(redis.client)
 [Queue][starlite_saqlalchemy.worker.Queue] instance instantiated with
 [redis][starlite_saqlalchemy.redis.client] instance.
 """
+
+
+@dataclasses.dataclass()
+class JobConfig:
+    """Configure a Job.
+
+    Used to configure jobs enqueued via
+    `Service.enqueue_background_task()`
+    """
+
+    # pylint:disable=too-many-instance-attributes
+
+    queue: Queue = queue
+    """Queue associated with the job."""
+    key: str | None = None
+    """Pass in to control duplicate jobs."""
+    timeout: int = settings.worker.JOB_TIMEOUT
+    """Max time a job can run for, in seconds.
+
+    Set to `0` for no timeout.
+    """
+    heartbeat: int = settings.worker.JOB_HEARTBEAT
+    """Max time a job can survive without emitting a heartbeat. `0` to disable.
+
+    `job.update()` will trigger a heartbeat.
+    """
+    retries: int = settings.worker.JOB_RETRIES
+    """Max attempts for any job."""
+    ttl: int = settings.worker.JOB_TTL
+    """Lifetime of available job information, in seconds.
+
+    0: indefinite
+    -1: disabled (no info retained)
+    """
+    retry_delay: float = settings.worker.JOB_TTL
+    """Seconds to delay before retrying a job."""
+    retry_backoff: bool | float = settings.worker.JOB_RETRY_BACKOFF
+    """If true, use exponential backoff for retry delays.
+
+    - The first retry will have whatever retry_delay is.
+    - The second retry will have retry_delay*2. The third retry will have retry_delay*4. And so on.
+    - This always includes jitter, where the final retry delay is a random number between 0 and the calculated retry delay.
+    - If retry_backoff is set to a number, that number is the maximum retry delay, in seconds."
+    """
+
+
+default_job_config_dict = utils.dataclass_as_dict_shallow(JobConfig(), exclude_none=True)
 
 
 def create_worker_instance(
