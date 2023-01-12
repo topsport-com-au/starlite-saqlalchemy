ed2fe59010d2cb580820f2b833bab00fe85a8eb2
Peter Schutt <peter.github@proton.me>
2023-01-12 10:50:07 +1000

    fix(server)!: fix reloading config (#231)
    * ✨ feat(scripts): auto reload when ENVIRONMENT==local
    
    * 🐛 fix: typing and docs
    
    * fix(reloading): use `uvicorn.run()`
    
    Logic for reloading is in `uvicorn.run()`, but to use it we need to let
    uvicorn manage the event loop for us.
    
    So this commit moves the database/cache readiness checks into a
    starlite, "before_startup" hook handler, so that they are conducted
    after uvicorn has setup the event loop.
    
    Co-authored-by: gazorby <gazorby@pm.me>

--- src/starlite_saqlalchemy/scripts.py
+++ src/starlite_saqlalchemy/scripts.py
@@ -1,66 +1,41 @@
 """Application startup script."""
-# pragma: no cover
-# pylint: disable=broad-except
-import argparse
-import asyncio
-
 import uvicorn
-import uvloop
-from sqlalchemy import text
 
-from starlite_saqlalchemy import redis, settings
-from starlite_saqlalchemy.db import engine
-
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+from starlite_saqlalchemy import settings
 
 
-async def _db_ready() -> None:
-    """Wait for database to become responsive."""
-    while True:
-        try:
-            async with engine.begin() as conn:
-                await conn.execute(text("SELECT 1"))
-        except Exception as exc:
-            print(f"Waiting for DB: {exc}")
-            await asyncio.sleep(5)
-        else:
-            print("DB OK!")
-            break
+def determine_should_reload() -> bool:
+    """Evaluate whether reloading should be enabled."""
+    return (
+        settings.server.RELOAD
+        if settings.server.RELOAD is not None
+        else settings.app.ENVIRONMENT == "local"
+    )
 
 
-async def _redis_ready() -> None:
-    """Wait for redis to become responsive."""
-    while True:
-        try:
-            await redis.client.ping()
-        except Exception as exc:
-            print(f"Waiting  for Redis: {exc}")
-            await asyncio.sleep(5)
-        else:
-            print("Redis OK!")
-            break
+def determine_reload_dirs(should_reload: bool) -> list[str] | None:
+    """
+
+    Args:
+        should_reload: is reloading enabled?
+
+    Returns:
+        List of directories to watch, or `None` if reloading disabled.
+    """
+    return settings.server.RELOAD_DIRS if should_reload else None
 
 
 def run_app() -> None:
-    """Run the application."""
-    parser = argparse.ArgumentParser(description="Run the application")
-    parser.add_argument("--no-db", action="store_const", const=False, default=True, dest="check_db")
-    parser.add_argument(
-        "--no-cache", action="store_const", const=False, default=True, dest="check_cache"
-    )
-    args = parser.parse_args()
-    with asyncio.Runner() as runner:
-        if args.check_db:
-            runner.run(_db_ready())
-        if args.check_cache:
-            runner.run(_redis_ready())
+    """Run the application with config via environment."""
+    should_reload = determine_should_reload()
+    reload_dirs = determine_reload_dirs(should_reload)
     uvicorn.run(
         app=settings.server.APP_LOC,
         factory=settings.server.APP_LOC_IS_FACTORY,
         host=settings.server.HOST,
-        loop="none",
+        loop="auto",
         port=settings.server.PORT,
-        reload=settings.server.RELOAD,
-        reload_dirs=settings.server.RELOAD_DIRS,
+        reload=should_reload,
+        reload_dirs=reload_dirs,
         timeout_keep_alive=settings.server.KEEPALIVE,
     )
