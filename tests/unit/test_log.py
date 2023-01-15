75a7db8a67236d0089cefc4f1630d64b2c0e154f
Peter Schutt <peter.github@proton.me>
2023-01-16 09:14:01 +1000

    chore(deps): update starlite
    Remove x-failed test fixed in starlite 1.49

--- tests/unit/test_log.py
+++ tests/unit/test_log.py
@@ -435,7 +435,6 @@ async def test_exception_in_before_send_handler_read_empty_body(
     assert "exception" not in call.kwargs
 
 
-@pytest.mark.xfail(reason="starlite is returning 500 for invalid payloads as of v1.48.1")
 async def test_log_request_with_invalid_json_payload(client: TestClient[Starlite]) -> None:
     """Test logs emitted with invalid client payload.
 
