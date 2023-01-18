2381cd7372ef42470e27959e09d47c4f3da7620a
Peter Schutt <peter.github@proton.me>
2023-01-18 13:01:41 +1000

    refactor: makes lifespan check imports and logic conditional

--- src/starlite_saqlalchemy/lifespan.py
+++ src/starlite_saqlalchemy/lifespan.py
@@ -1,5 +1,5 @@
 """Application lifespan handlers."""
-# pylint: disable=broad-except
+# pylint: disable=broad-except,import-outside-toplevel
 import asyncio
 import logging
 
@@ -7,48 +7,47 @@ import starlite
 
 from starlite_saqlalchemy import constants, settings
 
-if constants.IS_REDIS_INSTALLED:
-    from starlite_saqlalchemy import redis
-
-if constants.IS_SQLALCHEMY_INSTALLED:
-    from sqlalchemy import text
-
-    from starlite_saqlalchemy.db import engine  # pylint: disable=ungrouped-imports
-
-
 logger = logging.getLogger(__name__)
 
 
 async def _db_ready() -> None:
     """Wait for database to become responsive."""
-    while True:
-        try:
-            async with engine.begin() as conn:
-                await conn.execute(text("SELECT 1"))
-        except Exception as exc:
-            logger.info("Waiting for DB: %s", exc)
-            await asyncio.sleep(5)
-        else:
-            logger.info("DB OK!")
-            break
+    if constants.IS_SQLALCHEMY_INSTALLED:
+        from sqlalchemy import text
+
+        from starlite_saqlalchemy.db import engine
+
+        while True:
+            try:
+                async with engine.begin() as conn:
+                    await conn.execute(text("SELECT 1"))
+            except Exception as exc:
+                logger.info("Waiting for DB: %s", exc)
+                await asyncio.sleep(5)
+            else:
+                logger.info("DB OK!")
+                break
 
 
 async def _redis_ready() -> None:
     """Wait for redis to become responsive."""
-    while True:
-        try:
-            await redis.client.ping()
-        except Exception as exc:
-            logger.info("Waiting  for Redis: %s", exc)
-            await asyncio.sleep(5)
-        else:
-            logger.info("Redis OK!")
-            break
+    if constants.IS_REDIS_INSTALLED:
+        from starlite_saqlalchemy import redis
+
+        while True:
+            try:
+                await redis.client.ping()
+            except Exception as exc:
+                logger.info("Waiting  for Redis: %s", exc)
+                await asyncio.sleep(5)
+            else:
+                logger.info("Redis OK!")
+                break
 
 
 async def before_startup_handler(_: starlite.Starlite) -> None:
     """Do things before the app starts up."""
-    if constants.IS_SQLALCHEMY_INSTALLED and settings.app.CHECK_DB_READY:
+    if settings.app.CHECK_DB_READY:
         await _db_ready()
-    if constants.IS_REDIS_INSTALLED and settings.app.CHECK_REDIS_READY:
+    if settings.app.CHECK_REDIS_READY:
         await _redis_ready()
