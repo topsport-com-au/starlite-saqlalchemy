c915b16dbc666c9289a6f9974d855e309975f94c
Cody Fincher <codyfincher@google.com>
2023-01-26 16:41:32 -0600

    fix: adds redis configs

--- src/starlite_saqlalchemy/redis.py
+++ src/starlite_saqlalchemy/redis.py
@@ -7,7 +7,12 @@ from starlite_saqlalchemy import settings
 
 __all__ = ["client"]
 
-client: Redis[bytes] = Redis.from_url(settings.redis.URL)
+client: Redis[bytes] = Redis.from_url(
+    settings.redis.URL,
+    socket_connect_timeout=settings.redis.SOCKET_CONNECT_TIMEOUT,
+    health_check_interval=settings.redis.HEALTH_CHECK_INTERVAL,
+    socket_keepalive=settings.redis.SOCKET_KEEPALIVE,
+)
 """Async [`Redis`][redis.Redis] instance.
 
 Configure via [CacheSettings][starlite_saqlalchemy.settings.RedisSettings].
